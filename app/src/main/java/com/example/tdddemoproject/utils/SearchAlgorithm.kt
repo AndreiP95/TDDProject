9702ca7557e9ee0c8c8dff6a69896701cb419ea3
Alex Codreanu <alexandru.codreanu@itsmartsystems.eu>
2020-09-28 16:05:52 +0300

    Implemented Trie and Search Algorithm

--- app/src/main/java/com/example/tdddemoproject/utils/SearchAlgorithm.kt
+++ app/src/main/java/com/example/tdddemoproject/utils/SearchAlgorithm.kt
@@ -1,6 +1,7 @@
 package com.example.tdddemoproject.utils
 
 import com.example.tdddemoproject.repo.model.City
+import com.example.tdddemoproject.utils.trie.Trie
 
 /**
  * Calls a Trie - Prefix Tree - in order to display the cities that
@@ -10,6 +11,20 @@ import com.example.tdddemoproject.repo.model.City
  * @param cityToFind - The pattern written by the user
  * @return - A nullable list of cities that match the input written by the user
  */
-fun searchAlgorithm(cities: ArrayList<City>, cityToFind: String): ArrayList<City>? {
-    return null
+fun searchAlgorithm(cities: ArrayList<City>, cityToFind: String): ArrayList<City?> {
+    val citiesTrie = initTrie(cities)
+    return citiesTrie.findCitiesWith(cityToFind)
 }
+
+/**
+ * Initializes Trie and inserts the list of cities
+ *
+ * @param cities - The list of cities that will be parsed
+ * @return - an initialized Trie
+ */
+private fun initTrie(cities: ArrayList<City>): Trie {
+    val citiesTrie = Trie()
+    cities.forEach { citiesTrie.insert(it) }
+    return citiesTrie
+}
+
