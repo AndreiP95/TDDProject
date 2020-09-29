9544aa82f7e869334f9f61d220a305c7e3445216
AlexNicu <niculae.alexandru96@gmail.com>
2020-09-29 14:23:16 +0300

    SplashScreen fragment - added UI + error loading fragment UI  - loaded the json city list + sorted

--- app/src/main/java/com/example/tdddemoproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/tdddemoproject/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.tdddemoproject.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.tdddemoproject.R
+import com.example.tdddemoproject.ui.splashScreen.SplashScreenFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SplashScreenFragment.newInstance())
+                .commitNow()
+        }
     }
 }
