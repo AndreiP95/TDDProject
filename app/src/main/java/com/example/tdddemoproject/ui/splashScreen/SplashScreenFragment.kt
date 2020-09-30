59dbd26964515d8b60233de491a3ba381d036dff
AndreiPancu <andreipancu95@gmail.com>
2020-09-30 14:01:26 +0300

    Refactored splash screen + Added read method from file

--- app/src/main/java/com/example/tdddemoproject/ui/splashScreen/SplashScreenFragment.kt
+++ app/src/main/java/com/example/tdddemoproject/ui/splashScreen/SplashScreenFragment.kt
@@ -7,87 +7,57 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tdddemoproject.R
-import com.example.tdddemoproject.repo.model.City
-import com.example.tdddemoproject.utils.searchAlgorithm
-import com.example.tdddemoproject.utils.trie.Trie
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.example.tdddemoproject.repo.local.RetrieveCities
 import kotlinx.android.synthetic.main.splash_screen_fragment.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
-import java.io.InputStream
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlin.coroutines.CoroutineContext
 
 
 class SplashScreenFragment : Fragment(), CoroutineScope {
-    private lateinit var reader: InputStream
-    var cityJsonList: List<City>? = null
-    val action = SplashScreenFragmentDirections.actionGoToSearchScreen()
+    private val action = SplashScreenFragmentDirections.actionGoToSearchScreen()
     private var job: Job? = null
-    private val trie = inject<Trie>()
+    private val retrieveCities by inject<RetrieveCities>()
+    private lateinit var root: View
+    private val splashViewModel by viewModel<SplashScreenViewModel>()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.splash_screen_fragment, container, false)
-
-        // TODO -> Should use return value of loadCities instead of a local variable for the list
-
-        loadCities()
-        if (cityJsonList != null) {
-            initTrieList()
-            findNavController().navigate(action)
-        } else {
-            root.splash_screen_layout.visibility = View.GONE
-            root.loading_error_screen_layout.visibility = View.VISIBLE
-        }
-
+        root = inflater.inflate(R.layout.splash_screen_fragment, container, false)
         return root
     }
 
-    // TODO -> City loading should be on a different Thread
-    // TODO -> Move city loading to Repo package.
-
-    private fun loadCities(): List<City>? {
-        reader = resources.openRawResource(R.raw.cities)
-        var buffer: ByteArray? = ByteArray(reader.available())
-        while (reader.read(buffer) != -1) {
-        }
-        var text: String? = null
-        buffer?.let {
-            text = String(it)
-        }
-        // TODO -> list can be saved directly into an ArrayList without the need for conversion
-        // TODO -> From
-        cityJsonList = ObjectMapper().readValue<List<City>>(
-            text,
-            ObjectMapper().typeFactory.constructCollectionType(
-                List::class.java,
-                City::class.java
-            )
-        )
-        text = null
-        reader.close()
-        return cityJsonList
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        readData()
     }
 
-    fun initTrieList() {
-        var list: List<City> = cityJsonList as List<City>
-        var sortedCityList: List<City> = HashSet<City>(list).sortedBy { it.name }
-        var finaList: ArrayList<City>? = null
-        for (item in sortedCityList) {
-            finaList?.add(item)
-        }
-
-        var trieList = finaList?.let { Trie.initTrie(it) }
-
-        trieList?.let { searchAlgorithm(it, "") }
+    private fun showErrorScreen() {
+        root.splash_screen_layout.visibility = View.GONE
+        root.loading_error_screen_layout.visibility = View.VISIBLE
     }
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Default + (job ?: Job())
+
+
+    private fun readData() {
+        launch(Dispatchers.IO) {
+            val cities = retrieveCities.loadCities(this@SplashScreenFragment)
+            if (cities.isNullOrEmpty())
+                launch(Dispatchers.Main) { showErrorScreen() }
+            else {
+                splashViewModel.initTrieList(cities)
+                launch(Dispatchers.Main) { findNavController().navigate(action) }
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/tdddemoproject/ui/splashScreen/SplashScreenViewModel.kt
+++ app/src/main/java/com/example/tdddemoproject/ui/splashScreen/SplashScreenViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.tdddemoproject.ui.splashScreen
 
 import androidx.lifecycle.ViewModel
+import com.example.tdddemoproject.repo.model.City
+import com.example.tdddemoproject.utils.trie.Trie
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class SplashScreenViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SplashScreenViewModel : ViewModel(), KoinComponent {
+
+    private val trie by inject<Trie>()
+
+    fun initTrieList(cities: ArrayList<City>) {
+        val sortedCityList: List<City> = cities.sortedWith(
+            compareBy(
+                { it.name },
+                { it.country })
+        )
+        sortedCityList.let { trie.populateTrie(it) }
+    }
+
+
 }
